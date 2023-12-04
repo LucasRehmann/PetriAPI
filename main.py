eee79479961c64687632008a87dda880cbac9f31
Klaus <101809208+LucasRehmann@users.noreply.github.com>
2023-12-04 09:17:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["http://localhost"]
+origins = ["https://petrilab.vercel.app"]
 
 app.add_middleware(
     CORSMiddleware,
