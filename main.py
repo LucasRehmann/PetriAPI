f9232ff83d31de6f68a47de5b59b7274e4159c8b
Klaus <101809208+LucasRehmann@users.noreply.github.com>
2023-12-04 10:18:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["POST"],
     allow_headers=["Content-Type",  
                    "Access-Control-Allow-Credentials", 
                    "Access-Control-Allow-Metods"]
