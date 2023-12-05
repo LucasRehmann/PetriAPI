a4ff3f046dfc7ea183d4c4d2899a597de84d4ab2
Klaus <101809208+LucasRehmann@users.noreply.github.com>
2023-12-05 11:31:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# origins = ["https://petrilab.vercel.app"]
+origins = ["https://petrilab.vercel.app"]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["POST"],
-#     allow_headers=["Content-Type",  
-#                    "Access-Control-Allow-Credentials", 
-#                    "Access-Control-Allow-Metods"]
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST"],
+    allow_headers=["Content-Type",  
+                   "Access-Control-Allow-Credentials", 
+                   "Access-Control-Allow-Methods"]
+)
 
 @app.post("/process_image/")
 async def process_image(request: Request):
